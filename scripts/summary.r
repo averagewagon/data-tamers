3d594ae2c1fb3a518a07f053736ed76397fab482
VaneRaklan <anton.manuel.ramirez@gmail.com>
2019-05-21 14:55:51 -0700

    Add some comments for structure

---
+++ scripts/summary.r
@@ -0,0 +1,21 @@
+# General setup
+library("dplyr")
+library("ggplot2")
+
+# Summary Table
+
+
+
+# Chart 1: Fed Rev & State Rev
+
+
+
+# Chart 2: Salaries (Go into subcategories)
+
+
+
+# Chart 3: Total Expenditures
+
+
+
+
