d8b467404e1175099b599a2e1fa28e3bb3dd385f
lisicase <casee@uw.edu>
2019-05-24 12:35:21 -0700

    deleted content for summary table

--- scripts/summaryTable.R
+++ scripts/summaryTable.R
@@ -4,15 +4,3 @@ library("ggplot2")
 midpoint <- read.csv("data/midpoint.csv", stringsAsFactors = F)
 
 # Create aggregate table
-averages_per_year <- midpoint$prepFile %>%
-  group_by(year) %>%
-  summarize("Students" = ave([[Students]]),
-            "Total Revenue" = ave([[Total Revenue]], na.rm = T),
-            "Federal Revenue" = ave([[Federal Revenue]], na.rm = T),
-            "State Revenue" = ave([[State Revenue]], na.rm = T),
-            "Local Revenue" = ave([[Local Revenue]], na.rm = T),
-            "Total Expenses" = ave([[Total Expenses]], na.rm = T),
-            "Salaries" = ave([[Salaries]], na.rm = T),
-            "Teacher Revenue" = ave([[Teacher Revenue]], na.rm = T),
-            "Debt" = [[Total Expenses]] - [[Total Revenue]], na.rm = T
-  )
