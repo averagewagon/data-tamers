e1014c5fefb811bc82aff91c3f1b63e8072626f0
VaneRaklan <anton.manuel.ramirez@gmail.com>
2019-05-24 14:53:57 -0700

    Add chart1 function for total rev over the years, add chart1 to index

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -1,3 +1,17 @@
 # General setup
 library("dplyr")
 library("ggplot2")
+
+
+generateTotalFunding <- function(filePath) {
+  #setwd(getSrcDirectory()[1]) # Works when sourcing this file
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Works when ran inside of this file
+  midpoint <- read.csv(filePath, stringsAsFactors = FALSE)
+  midpoint <- midpoint %>%
+    group_by(Year) %>%
+    summarize(Total.Revenue = sum(Total.Revenue))
+  ggplot() + 
+    geom_col(data = midpoint, aes(x = Year, y = Total.Revenue / 1000)) +
+    xlab('Years') +
+    ylab('Total Revenue (thousands)')
+}
