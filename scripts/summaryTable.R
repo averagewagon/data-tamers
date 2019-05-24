5289165c4f23652cd4c4bd1ac0c5f84d1ea852cb
johnrhen <john.r.hendric@gmail.com>
2019-05-24 13:38:35 -0700

    Complete summary table

--- scripts/summaryTable.R
+++ scripts/summaryTable.R
@@ -1,6 +1,16 @@
 # General setup
 library("dplyr")
 library("ggplot2")
-midpoint <- read.csv("data/midpoint.csv", stringsAsFactors = F)
 
-# Create aggregate table
+aggregated <- read.csv("../data/funding/prepped/aggregate.csv", stringsAsFactors = FALSE)
+
+yearly <- aggregated %>% 
+  group_by(Year) %>% 
+  summarise("Students" = sum(Students),
+            "Total Revenue" = sum(Total.Revenue),
+            "Federal Revenue" = sum(Federal.Revenue),
+            "State Revenue" = sum(State.Revenue),
+            "Local Revenue" = sum(Local.Revenue),
+            "Total Expenditures" = sum(Total.Expenditures),
+            "Salaries" = sum(Salaries),
+            "Teacher Revenue" = sum(Teacher.Revenue))
