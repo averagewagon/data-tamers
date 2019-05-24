fb56ce22c6a3b3a5919a6fe50d97dccffdbe773a
lisicase <casee@uw.edu>
2019-05-24 15:55:05 -0700

    revised function format for better behavior

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -2,33 +2,36 @@
 library("dyplr")
 library("ggplot2")
 
-# Load shapefile of U.S. states
-state_shape <- map_data("state")
-
-# Create a blank map of U.S. states
-usa_map <- ggplot(state_shape) +
+# Draw the map showing state surplus amount
+surplus_map <- function(given_year) {
+  # Load shapefile of U.S. states
+  state_shape <- map_data("state")
+  # Create a blank map of U.S. states
+  usa_map <- ggplot(state_shape) +
+    geom_polygon(
+      mapping = aes(x = long, y = lat, group = group),
+      color = "white",
+      size = .1
+    ) +
+    coord_map()
+  # Load funding data
+  funding_data <- funding_data(given_year)
+  # Join eviction data to the U.S. shapefile
+  state_shape <- map_data("state") %>%
+    rename(state = region) %>%
+    left_join(funding_data, by = "state")
+  # Fill in the map
+  ggplot(state_shape) +
   geom_polygon(
-    mapping = aes(x = long, y = lat, group = group),
+    mapping = aes(x = long, y = lat, group = group, fill = surplus),
     color = "white",
     size = .1
   ) +
-  coord_map()
-
-# Load funding data
-funding_data <- function(given_year) {
-  read.csv("../data/funding/prepped/aggregate.csv", stringsAsFactors = F) %>%
-  filter(Year == given_year) %>%
-  mutate(state = tolower(State.Name)) %>%
-  mutate(surplus = Total.Revenue - Total.Expenditures)
-}
-
-# Join eviction data to the U.S. shapefile
-state_shape <- map_data("state") %>%
-  rename(state = region) %>%
-  left_join(funding, by = "state")
-
-# Define a minimalist theme for maps
-minimalist_theme <- theme_bw() +
+  coord_map() +
+  scale_fill_continuous(low = "Red", high = "Yellow") +
+  labs(fill = paste(given_year, "Surplus ($)")) +
+  # Add a minimalist theme
+  theme_bw() +
   theme(
     axis.line = element_blank(),
     axis.text = element_blank(),
@@ -39,18 +42,12 @@ minimalist_theme <- theme_bw() +
     panel.grid.minor = element_blank(),
     panel.border = element_blank()
   )
+}
 
-# Draw the map showing state surplus amount
-surplus_map <- function(given_year) {
-  funding_data(given_year)
-  ggplot(state_shape) +
-  geom_polygon(
-    mapping = aes(x = long, y = lat, group = group, fill = surplus),
-    color = "white",
-    size = .1
-  ) +
-  coord_map() + # use a map-based coordinate system
-  scale_fill_continuous(low = "Red", high = "Yellow") +
-  labs(fill = "Surplus ($)") +
-  minimalist_theme
+# Load state funding data for a given year
+funding_data <- function(given_year) {
+  read.csv("../data/funding/prepped/aggregate.csv", stringsAsFactors = F) %>%
+    filter(Year == given_year) %>%
+    mutate(state = tolower(State.Name)) %>%
+    mutate(surplus = Total.Revenue - Total.Expenditures)
 }
