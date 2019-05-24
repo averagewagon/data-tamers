06566e975634839dad00a66f24d26801ddd04196
lisicase <casee@uw.edu>
2019-05-24 16:38:51 -0700

    made chart 3 into a workable function

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -6,12 +6,12 @@ source("summaryTable.R")
 # Read in summary table
 divergingMap <- function(df) {
   # Create new column to color the vaules
-  df$Spending <- ifelse(df$"Total Revenue" < df$"Total Expenditures"
-                            , "debt","surplus")
+  df$Spending <- ifelse(df$"Total Revenue" < df$"Total Expenditures",
+                        "debt","surplus")
   # Create new column to show amounts of surplus or debt
   df$Spending_Amount <- df$'Total Revenue' - df$'Total Expenditures'
   # Diverging Barcharts
-  ggplot(yearly, aes(x = Year, y = Spending_Amount, label = Spending)) + 
+  ggplot(df, aes(x = Year, y = Spending_Amount, label = Spending)) + 
     geom_bar(stat='identity', aes(fill=Spending), width=.5)  +
     scale_fill_manual(name="Yearly School Spending", 
                       labels = c("Debt", "Surplus"), 
