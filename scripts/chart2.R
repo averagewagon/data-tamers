0692d9423a6cbbf0d6305ee8b33a08a9c1c56d4d
lisicase <casee@uw.edu>
2019-05-24 15:29:03 -0700

    created map of state surplus

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,23 +1,50 @@
 # General setup
-library(tidyverse)
-library(mapdata)
-#library(maps)
-library(stringr)
-library(viridis)
-library(viridisLite)
+library(ggplot2)
 
-funding_of_year <- read.csv("../data/funding/prepped/prepf2016.csv", stringsAsFactors = FALSE)
+# Load shapefile of U.S. states
+state_shape <- map_data("state")
 
-states <- map_data("state")
-states_base <- ggplot(data = states) + 
-  geom_polygon(aes(x = long, y = lat, fill = region, group = group)) + 
-  coord_quickmap() +
-  guides(fill = FALSE)  # do this to leave off the color legend
+# Create a blank map of U.S. states
+usa_map <- ggplot(state_shape) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group),
+    color = "white",
+    size = .1
+  ) +
+  coord_map()
 
-funds_use <- states_base + 
-  geom_polygon(data = funding_of_year, aes(fill = Total.Revenue - Total.Expenditures), color = "black") +
-  geom_polygon(color = "black", fill = NA) +
-  theme_void() +
-  scale_fill_gradientn(colours = rev(rainbow(2)),
-                       breaks = 0,
-                       trans = "log10")
+# Load funding data
+funding <- read.csv("../data/funding/prepped/aggregate.csv", stringsAsFactors = F) %>%
+  filter(Year == 2016) %>% # keep only 2016 data
+  mutate(state = tolower(State.Name)) %>%
+  mutate(surplus = Total.Revenue - Total.Expenditures)
+
+# Join eviction data to the U.S. shapefile
+state_shape <- map_data("state") %>%
+  rename(state = region) %>%
+  left_join(funding, by = "state")
+
+# Define a minimalist theme for maps
+minimalist_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    plot.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank()
+  )
+
+# Draw the map showing state surplus amount
+surplus_map <- ggplot(state_shape) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = surplus),
+    color = "white",
+    size = .1
+  ) +
+  coord_map() + # use a map-based coordinate system
+  scale_fill_continuous(low = "Red", high = "Yellow") +
+  labs(fill = "Surplus ($)") +
+  minimalist_theme
