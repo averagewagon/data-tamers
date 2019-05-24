efe513d17ba7171451370f3cd8c703aabe5116e0
VaneRaklan <anton.manuel.ramirez@gmail.com>
2019-05-24 13:13:23 -0700

    Add function to generate trend lines

--- scripts/chart4.R
+++ scripts/chart4.R
@@ -3,3 +3,14 @@
 # General setup
 library("dplyr")
 library("ggplot2")
+
+generateTrends <- function(filePath) {
+  midpoint <- read.csv(filePath, stringsAsFactors = FALSE)
+  
+  ggplot() + 
+    geom_line(data = midpoint, aes(x = Year, y = "Total Federal Revenue"), color = "blue") +
+    geom_line(data = midpoint, aes(x = Year, y = "Total State Revenue"), color = "red") +
+    geom_line(data = midpoint, aes(x = Year, y = "Total Local Revenue"), color = "green") +
+    xlab('Years') +
+    ylab('Revenue From Sources')
+}
