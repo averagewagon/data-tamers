9b613db119240980ea2049a84862bdcc4045746c
lisicase <casee@uw.edu>
2019-05-23 20:11:41 -0700

    added template for summary information

--- scripts/summaryInformation.R
+++ scripts/summaryInformation.R
@@ -4,5 +4,12 @@ library("ggplot2")
 
 # Summary information
 
-
-
+get_funding_summary_info <- function(dataset) {
+  funding_sum <- list()
+  funding_sum$num_districts <- length(dataset)
+  # 2. funding_sum$____ <- _____
+  # 3. funding_sum$____ <- _____
+  # 4. funding_sum$____ <- _____
+  # 5. funding_sum$____ <- _____
+  funding_sum
+}
