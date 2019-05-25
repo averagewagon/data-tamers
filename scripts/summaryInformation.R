aa2db788323075a1fce58d9c6e3aedcd09a1cb1f
YOUR_USERNAME <john.r.hendric@gmail.com>
2019-05-24 21:47:23 -0700

    Finish summary info

--- scripts/summaryInformation.R
+++ scripts/summaryInformation.R
@@ -5,15 +5,15 @@ library("ggplot2")
 # Summary information
 
 get_funding_summary_info <- function(df) {
-  funding_sum <- list(
-    num_years = length(df)
-    most_funded = get_most_funded(df)
-    # 3. ____ = ____
-    # 4. ____ = ____
-    # 5. ____ = ____
+ funding_sum <- list(
+   num_years = nrow(df),
+   total_revenue = sum(df$"Total Revenue"),
+   federal_cont = 100 * sum(df$"Federal Revenue") /sum(df$"Total Revenue"),
+   state_cont = 100 * sum(df$"State Revenue") /sum(df$"Total Revenue"),
+   local_cont = 100 * sum(df$"Local Revenue") /sum(df$"Total Revenue"),
+   average_salaries = formatC(mean(df$Salaries) / 51, format="d", big.mark=",")
   )
 }
 
-get_most_funded <- function(df) {
-  df 
-}
+
+  
