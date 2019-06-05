161af649c2b9f5471682ff3a9592d3c2754be9ea
YOUR_USERNAME <john.r.hendric@gmail.com>
2019-06-05 01:34:09 -0700

    Fix lintr errors

--- app_ui.R
+++ app_ui.R
@@ -9,11 +9,11 @@ library(shinythemes)
 # Overview sidebar
 overview_panel <- mainPanel(
   h4(id = "fh4", "The Data Tamers' Group Project"),
-  
+
   tags$hr(),
-  
+
   h3(id = "fh3", "The Data Set"),
-  
+
   div(id = "wrapper",
     div(id = "main",
       p(
@@ -24,38 +24,38 @@ overview_panel <- mainPanel(
         "this project centers around
           is the data from the \"Local Education Agency (School District)
           Finance Survey\", also known as
-          survey F-33. This survey is conducted yearly by the federal 
+          survey F-33. This survey is conducted yearly by the federal
           government of the USA, and aggregates information
-          on salaries, debt, student numbers, funding, expenditures, 
+          on salaries, debt, student numbers, funding, expenditures,
           and other monetary figures for all primary
-          and secondary schools. Each school district in the entirety 
-          of the USA is represented. For our analysis, we used 
+          and secondary schools. Each school district in the entirety
+          of the USA is represented. For our analysis, we used
           the publicly released data, which spans from 1995 up to 2016."
       ),
-      
+
       tags$hr(),
-      
-      p("To look at the data, we are using compiled datasets 
+
+      p("To look at the data, we are using compiled datasets
           from the U.S. Census Bureau as authorized
-          by Title 13, United States Code, Section 161 and 182. 
+          by Title 13, United States Code, Section 161 and 182.
           Due to the large volume of data, we narrowed our analysis down to
           columns for funding sources, salaries, and basic expenditures, and
           the data are grouped by state."),
-      
+
       tags$hr(),
-      
+
       p("Since the population in each state varies widely, our data is
         normalized according to student counts.",
-        
-        tags$strong(" A unit of money 
-        in this data set is not measured in USD, 
+
+        tags$strong(" A unit of money
+        in this data set is not measured in USD,
         but instead USD per student."),
-        
+
         " There are numerous interesting trends that arise in this data set,
         but we chose to focus on a few key insights for this analysis."
       )
     ),
-    
+
     div(
       tags$img(src = "federalrevenue.gif", width = "100%"),
       tags$em("Federal primary/secondary school funding from 1995 to 2016.")
@@ -65,21 +65,24 @@ overview_panel <- mainPanel(
 
 overview_sidebar_panel <- sidebarPanel(
   h3("The Questions"),
-  
+
   tags$ul(
-    tags$li("Over the 20 year period, how do the different metric change across the states?"), 
-    tags$li("Which states went into debt over the different years?"), 
-    tags$li("What is the distribution of each metric for each state for a given year?")
+    tags$li("Over the 20 year period, how do the different
+            metric change across the states?"),
+    tags$li("Which states went into debt over the
+            different years?"),
+    tags$li("What is the distribution of each metric for
+            each state for a given year?")
   )
 )
 
 overview_page <- tabPanel(
   "Overview",
   titlePanel("USA Education Funding over the years."),
-  
+
   sidebarLayout(
     overview_panel,
-    
+
     overview_sidebar_panel
   )
 )
@@ -114,10 +117,14 @@ map_sidebar_content <- sidebarPanel(
   ),
   tags$ul(
     "Insights from the graph:",
-    tags$li("Earlier on the total revenue for the country seemed to move relatively uniformly
-            until the northeast and Wyoming shot ahead of the other states."),
-    tags$li("Somewhere around 2010 there was a big spike in funding from federal resources. Aside
-            from Nevada and Colorado, all of the states visually brighten on the graph then dampens
+    tags$li("Earlier on the total revenue for
+    the country seemed to move relatively uniformly
+            until the northeast and Wyoming shot ahead
+            of the other states."),
+    tags$li("Somewhere around 2010 there was a big spike
+    in funding from federal resources. Aside
+            from Nevada and Colorado, all of the states
+            visually brighten on the graph then dampens
             back down after 2010 passes.")
   )
 
@@ -154,7 +161,7 @@ bar_sidebar_content <- sidebarPanel(
     value = 1995,
     step = 1
   ),
-  
+
   # Select dropdown for which state to display
   selectInput(
     "bar_state",
@@ -214,13 +221,13 @@ bar_sidebar_content <- sidebarPanel(
     ),
     selected = "AL"
   ),
-  
+
   tags$br(),
-  
+
   p("This chart shows how total revenue of a given state compares with
    the total expenditures of that state over the years. Notably, nearly
-   every state spends more than their revenue most years, meaning that they accrue
-   debt.")
+   every state spends more than their revenue most years,
+   meaning that they accrue debt.")
 )
 
 # the Main Panel, which holds the graph for population
@@ -307,9 +314,9 @@ div_sidebar_content <- sidebarPanel(
     ),
     selected = "AL"
   ),
-  
+
   tags$br(),
-  
+
   p("This chart displays the various divisions of funds for a single state.
     Notably, the federal government contributes the least out to primary
     and secondary schools out of any funding institution. As for funding,
@@ -324,16 +331,16 @@ div_main_content <- mainPanel(
 # populations tab panel, which has the pops sidebar and main content.
 div_panel <- tabPanel(
   "Debt and Surplus",
-  
+
   # Title for the pops panel
   titlePanel("Debt Calculations"),
-  
+
   # Sidebar layout
   sidebarLayout(
-    
+
     # Controls that change graph content
     div_sidebar_content,
-    
+
     # The bar graph for pops, split by county
     div_main_content
   )
@@ -349,7 +356,7 @@ summary_sidebar_content <- sidebarPanel(
             few exceptions, states such as Connecticut
             which only has debt for 2010. Leaving the
             schools to borrow from banks, issue bonds,
-            and teachers often pay for supplies for their 
+            and teachers often pay for supplies for their
             classes."),
     tags$li(""),
     tags$li("")
@@ -358,8 +365,8 @@ summary_sidebar_content <- sidebarPanel(
 
 summary_main_content <- mainPanel(
   h4("What are we taking away from this project?"),
-  p("Among the different metrics, 
-    there were many interesting things that 
+  p("Among the different metrics,
+    there were many interesting things that
     happened over the two decades we had available.
     The data showed there was a spike in federal funding
     for schools across the board in 2010. Some states
@@ -371,16 +378,16 @@ summary_main_content <- mainPanel(
 # populations tab panel, which has the pops sidebar and main content.
 summary_panel <- tabPanel(
   "Summary",
-  
+
   # Title for the pops panel
   titlePanel("Summary Information and Takeaways"),
-  
+
   # Sidebar layout
   sidebarLayout(
-    
+
     # Controls that change graph content
     summary_sidebar_content,
-    
+
     # The bar graph for pops, split by county
     summary_main_content
   )
