c0a61e256ff4395eb47aeba9d1f2e2acf04d49f4
VaneRaklan <anton.manuel.ramirez@gmail.com>
2019-06-04 17:25:41 -0700

    Work on overview; add tab for div bar and summary

--- app_ui.R
+++ app_ui.R
@@ -3,24 +3,42 @@
 library(shiny)
 library(plotly)
 
+# ==== Overview ====
+
 # Overview sidebar
 overview_panel <- mainPanel(
-
-  h2("The Data Tamers' Group Project"),
-  
-  tags$hr(),
-  
-  tags$hr(),
-  
-  h3("The Data Set"),
-  
-  p("blah")
+  HTML("<h2>The Data Tamers' Group Project</h2>
+        <hr><hr>
+        <h3>The Data Set</h3>
+        <p>
+          For this project, we are looking at the domain of education with a focus on the financial<br>
+          characteristics of schools over the past twenty years and how they has differ over time<br>
+          and geographic location.
+        </p>
+        <p>
+          To look into this, we are using compiled datasets from the U.S. Census Bureau as authorized<br>
+          by Title 13, United States Code, Section 161 and 182. Under this authorization, we are able<br>
+          to access surveys of every school in the United States, including Washington D.C., in order<br>
+          to collect information on education finance data such as revenue, expenditure, debt, and<br>
+          assets of elementary and secondary public school systems.
+        </p>
+        <p>
+          When we started this project we decided to dive into education data with the hopes of seeing<br>
+          how time, geography, and social/political/historical events affected the data. Even though<br>
+          we found data to suppart us on this journey (student incarceration data and school<br>
+          shootings data) we got too enveloped within the mass amount of financial data we gathered.
+        </p>
+       ")
 )
 
 overview_sidebar_panel <- sidebarPanel(
-  h3("The Questions"),
-  
-  p("blah")
+  HTML("<h3>The Questions</h3>
+        <ul>
+          <li>Over the 20 year period, how do the different metric change across the states?</li>
+          <li>Which states went into debt over the different years?</li>
+          <li>What is the distribution of each metric for each state for a given year?</li>
+        </ul>
+       ")
 )
 
 overview_page <- tabPanel(
@@ -33,6 +51,8 @@ overview_page <- tabPanel(
     overview_sidebar_panel
   )
 )
+
+# ==== Map ====
 
 # Sidebar for the map page
 map_sidebar_content <- sidebarPanel(
@@ -82,6 +102,8 @@ map_panel <- tabPanel(
     map_main_content
   )
 )
+
+# ==== Bar ====
 
 # Sidebar for the population information page
 bar_sidebar_content <- sidebarPanel(
@@ -136,10 +158,70 @@ bar_panel <- tabPanel(
   )
 )
 
+# ==== Summary ====
+
+div_sidebar_content <- sidebarPanel(
+  
+)
+
+div_main_content <- mainPanel(
+  
+)
+
+# populations tab panel, which has the pops sidebar and main content.
+div_panel <- tabPanel(
+  "Diverging Bar Chart",
+  
+  # Title for the pops panel
+  titlePanel("Debt Calculations"),
+  
+  # Sidebar layout
+  sidebarLayout(
+    
+    # Controls that change graph content
+    div_sidebar_content,
+    
+    # The bar graph for pops, split by county
+    div_main_content
+  )
+)
+
+# ==== Summary ====
+
+summary_sidebar_content <- sidebarPanel(
+  
+)
+
+summary_main_content <- mainPanel(
+  
+)
+
+# populations tab panel, which has the pops sidebar and main content.
+summary_panel <- tabPanel(
+  "Summary",
+  
+  # Title for the pops panel
+  titlePanel("Summary Information and Takeaways"),
+  
+  # Sidebar layout
+  sidebarLayout(
+    
+    # Controls that change graph content
+    summary_sidebar_content,
+    
+    # The bar graph for pops, split by county
+    summary_main_content
+  )
+)
+
+# ==== ui initializing ====
+
 # Final UI definition
 ui <- navbarPage(
-  "Midwest Population",
+  "Education Funding",
   overview_page,
   map_panel,
-  bar_panel
+  bar_panel,
+  div_panel,
+  summary_panel
 )
